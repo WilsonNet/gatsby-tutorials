5614bf80e62b1321d32246822573368587f238b1
Wilson Neto <wilsonpsn@hotmail.com>
2020-08-14 22:57:21 -0300

    Querying from the api

--- gatsby-graphql/src/pages/index.js
+++ gatsby-graphql/src/pages/index.js
@@ -8,12 +8,41 @@ import SEO from "../components/seo"
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>
+      This is a GraphQl simple tutorial from The Opinionated Guide To React by
+      Sara Vieras
+    </h1>
+    <p>Here are some users from Random Users</p>
+    <ul
+      style={{
+        listStyleType: "none",
+        display: "flex",
+        content: "space-between",
+        flexWrap: "wrap",
+      }}
+    >
+      {data.allRandomUser.edges.map(({ node }) => (
+        <li
+          style={{
+            flexShrink: 0,
+            marginLeft: '40px',
+            marginRight: '40px',
+            width: '100%',
+            display: "flex",
+            alignItems: "center",
+            border: "2px solid rgba(0, 0, 0, 0.5)",
+            maxWidth: "300px",
+          }}
+        >
+          <img
+            src={node.picture.thumbnail}
+            style={{ display: "block", marginRight: "10px" }}
+            alt={node.name.first}
+          />
+          {node.name.first} {node.name.last}
+        </li>
+      ))}
+    </ul>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
