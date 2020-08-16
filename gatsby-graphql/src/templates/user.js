07c9899b9121a30a8744a128e9f995b76b39ab6f
Wilson Neto <wilsonpsn@hotmail.com>
2020-08-15 21:37:54 -0300

    Now it has pages

--- gatsby-graphql/src/templates/user.js
+++ gatsby-graphql/src/templates/user.js
@@ -1,7 +1,23 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const User = props => {
-  return <>Hello</>
+const User = ({ data }) => {
+  const user = data.allRandomUser.edges[0].node
+  return <div>Hello {user.name.first} </div>
 }
 
 export default User
+
+export const query = graphql`
+  query User($id: String) {
+    allRandomUser(filter: { id: { eq: $id } }) {
+      edges {
+        node {
+          name {
+            first
+          }
+        }
+      }
+    }
+  }
+`
